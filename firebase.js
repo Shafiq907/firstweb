8bfc51fed8c9f5742470315745569504f062396f
Shafiq907 <mshafiq907@gmail.com>
2025-07-22 09:53:54 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs
+// We'll use CDN for simpler hosting (no bundler needed)
+const firebaseConfig = {
+  apiKey: "AIzaSyB2tkaajsK2d0eY6lXztK5nMpI8gVY3Vbg",
+  authDomain: "gibs-login-system.firebaseapp.com",
+  projectId: "gibs-login-system",
+  storageBucket: "gibs-login-system.firebasestorage.app",
+  messagingSenderId: "1065767063573",
+  appId: "1:1065767063573:web:42a7373ae0e7fff770fe1c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
